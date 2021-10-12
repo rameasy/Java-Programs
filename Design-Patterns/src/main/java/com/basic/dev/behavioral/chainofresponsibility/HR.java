1b0b64cc3d74edc1557e896c5294e59cd4df489b
Ramachandran Subramanian <rameasy@gmail.com>
2021-10-12 14:25:38 +0100

    Test commit

--- Design-Patterns/src/main/java/com/basic/dev/behavioral/chainofresponsibility/HR.java
+++ Design-Patterns/src/main/java/com/basic/dev/behavioral/chainofresponsibility/HR.java
@@ -5,7 +5,7 @@ public class HR extends LeaveHandler {
 	@Override
 	public String applyLeave(Leave leave) {
 
-		// HR can approve up to 21 days, otherwise it will pass to the Manager
+		// HR can try to approve up to 21 days, otherwise it will pass to the Manager
 		if (leave.getNumberOfDays() <= 21) {
 			// Employee tier should be 3 or above & reason type should not be "Regular" to
 			// get approved
